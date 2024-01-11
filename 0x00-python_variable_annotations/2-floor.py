0dfb668020b89aa7913b7512dcf8125b98bae0b8
Safuan04 <elmai.safuan@gmail.com>
2024-01-11 15:36:59 +0100

    2. Basic annotations - floor

---
+++ 0x00-python_variable_annotations/2-floor.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+"""Defining floor function"""
+
+
+def floor(n: float) -> int:
+    """This is a function that takes a float n as argument
+        and returns the floor of the float."""
+    i = 0
+
+    while i <= n:
+        i += 1
+
+    return i - 1
