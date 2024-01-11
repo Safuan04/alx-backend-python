bc9d224ae240b818d9e269ef59c16b594750088a
Safuan04 <elmai.safuan@gmail.com>
2024-01-11 15:49:53 +0100

    3. Basic annotations - to string

---
+++ 0x00-python_variable_annotations/3-to_str.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python3
+"""Defining to_str function"""
+
+
+def to_str(n: float) -> str:
+    """This is a function that takes a float n as argument
+        and returns the string representation of the float."""
+    return str(n)
