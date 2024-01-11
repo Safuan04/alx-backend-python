345839bec584074bbf65793332d7b6dab49f16c8
Safuan04 <elmai.safuan@gmail.com>
2024-01-11 15:19:32 +0100

    1. Basic annotations - concat

--- 0x00-python_variable_annotations/1-concat.py
+++ 0x00-python_variable_annotations/1-concat.py
@@ -3,6 +3,6 @@
 
 
 def concat(str1: str, str2: str) -> str:
-  """This is a function that takes a string str1 and a string str2
-    as arguments and returns a concatenated string"""
-  return str1 + str2
+    """This is a function that takes a string str1 and a string str2
+        as arguments and returns a concatenated string"""
+    return str1 + str2
